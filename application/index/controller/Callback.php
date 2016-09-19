b61ca87c626e814968b3e6be42d275b7319c9243
pydo <964683112@qq.com>
2016-09-19 08:42:42 +0800

    add callback

---
+++ application/index/controller/Callback.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xpro
+ * Date: 2016/9/19 0019
+ * Time: 08:28
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class Callback
+{
+
+}

d7e0a3840e2047f973376da0859afea43fbd4061
pydo <964683112@qq.com>
2016-09-19 13:41:53 +0800

    2

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -13,7 +13,7 @@ class Index extends  Controller
         if(session('?user')){
             return 'welcom you:'.session('user');
         }else{
-            dump(session);
+           // dump(session);
             $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcebdc16fde18fb3c&redirect_uri=http://www.eeyi.org/rainyrainy/public/index.php/index/callback/index&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
             return redirect($url);
         }

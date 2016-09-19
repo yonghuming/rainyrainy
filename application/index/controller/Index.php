270332d3bd5308899051d03599f696c50ea83c6d
pydo <964683112@qq.com>
2016-09-19 11:28:51 +0800

    修复资源文件加载在没有配置虚拟域名时候的加载问题

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,7 +1,9 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends  Controller
 {
     public function index()
     {
@@ -11,5 +13,8 @@ class Index
             $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcebdc16fde18fb3c&redirect_uri=http://www.eeyi.org/rainyrainy/public/index.php/index/callback/index&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
             return redirect($url);
         }
+    }
+    public function js(){
+        return $this->fetch();
     }
 }
